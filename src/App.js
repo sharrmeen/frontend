8491bd51845d8ab0287fa335f301b7eda14e8847
sharrmeen <sharmeenk2020@gmail.com>
2024-04-01 00:17:38 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
-import Main from "./Components/Home/Main"
+import "./App.css";
+
+//Importing necessary components amd functions from the react-router-dom library for routing.
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Main from "./Components/Home/Main";
 import PropertyList from "./Components/Home/PropertyList";
 import PropertyDetails from "./Components/PropertyDetails/PropertyDetails";
-import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
-
-
-
+import Login from "./Components/User/Login";
+import { Flip, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { currentUser } from "./Store/User/user-action";
+import { userActions } from "./Store/User/user-slice";
+// import Signup from "./Components/User/Signup";
 
 function App() {
+  const dispatch = useDispatch();
+  const { errors } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (errors) {
+      dispatch(userActions.clearError());
+    }
+    dispatch(currentUser());
+  }, [errors, dispatch]);
 
+  //mananges the routing configuration for the application.
   const router = createBrowserRouter(
+    // creates routes from the elements passed to it.
     createRoutesFromElements(
-      //defines a Route component that matches all paths "/" and renders the Main component
-      //exact properties ensurethat the route matches exactly what you gave in path
-      <Route path ="/" element={<Main/>} id="main" exact>
-      <Route id="home" index element={<PropertyList/>}exact/>
-      <Route
+      //defines a Route component that matches all paths "/" and renders the Main component.
+      //exact properties ensure that the route matches exactly what u gave in path
+      <Route path='/' element={<Main />} id='main' exact>
+        <Route id='home' index element={<PropertyList />} exact />
+        <Route
           element={<PropertyDetails />}
           id='PropertyDetails'
           path='propertylist/:id'
           exact
         />
+        <Route id='login' path='login' element={<Login />} />
+        {/* <Route id='signup' path='signup' element={<Signup />} /> */}
       </Route>
-
     )
   );
-  return(
-     <div className="App">
-    {/* This ensures that the routing functionality is available throughout the application */}
-    <RouterProvider router={router} />
-  </div>
+  return (
+    <div className='App'>
+      {/* This ensures that the rounting functionallity is available throughout the application */}
+      <RouterProvider router={router} />
+      <ToastContainer
+        position='bottom-center'
+        autoClose={3000}
+        draggable={true}
+        transition={Flip}
+      />
+    </div>
   );
 }
-
 
 export default App;
